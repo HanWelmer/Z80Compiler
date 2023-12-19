6434c4e867c71e16400f0d0b40fa6de94bb02ddc
HanOfBea <han.welmer@tno.nl>
2023-12-20 00:38:07 +0100

    Added unit tests regarding modifiers in TypeDeclaration.

---
+++ src/test/resources/expected/ClassDeclaration/TestClassModifierDefault.exp
@@ -0,0 +1,11 @@
+   0 ;TestClassModifierDefault.j(0) /*
+   1 ;TestClassModifierDefault.j(1)  * A small program in the miniJava language.
+   2 ;TestClassModifierDefault.j(2)  * Test ClassDeclaration without modifier.
+   3 ;TestClassModifierDefault.j(3)  */
+   4 ;TestClassModifierDefault.j(4) class TestClassModifierDefault {
+   5 ;TestClassModifierDefault.j(5)   println("Klaar");
+   6 acc16= constant 10
+   7 writeLineString
+   8 ;TestClassModifierDefault.j(6) }
+   9 stop
+  10 stringConstant 0 = "Klaar"
---
+++ src/test/resources/mCode/ClassDeclaration/TestClassModifierPublic.m
@@ -0,0 +1,11 @@
+   0 ;TestClassModifierPublic.j(0) /*
+   1 ;TestClassModifierPublic.j(1)  * A small program in the miniJava language.
+   2 ;TestClassModifierPublic.j(2)  * Test ClassDeclaration with public modifier.
+   3 ;TestClassModifierPublic.j(3)  */
+   4 ;TestClassModifierPublic.j(4) public class TestClassModifierPublic {
+   5 ;TestClassModifierPublic.j(5)   println("Klaar");
+   6 acc16= constant 10
+   7 writeLineString
+   8 ;TestClassModifierPublic.j(6) }
+   9 stop
+  10 stringConstant 0 = "Klaar"
